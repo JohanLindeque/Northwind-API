11248f94174556970379323e9f2a54cc7eb2d260
Johan Lindeque <johanlindeque34@gmail.com>
2025-09-29 19:15:34 +0200

    add seperate DbContext for existing DB and Auth

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,4 +1,6 @@
 using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Northwind_API.Models.Models;
 
